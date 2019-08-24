cb70fdcf3e9a65b200abb8796d8ce60f1552040c
b_zhigzhitov <b.zhigzhitov@text.ru>
2019-08-24 17:33:30 +0300

    Просто коммит

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -26,5 +29,29 @@ class DefaultController extends Controller
             $params['balances'] = $wallet->getCurrencies($user);
         }
         return $this->render('default/index.html.twig', $params);
+    }
+
+    /**
+     * PHPINFO function
+     */
+    public function phpinfo()
+    {
+        echo phpinfo();
+    }
+
+    /**
+     * @return Response
+     */
+    public function json1()
+    {
+        $user = $this->getUser();
+        $params = [];
+        if (isset($user)) {
+            $params['username'] = $user->getUsername();
+            $wallet = $this->get('app.wallet_manager');
+            $params['balances'] = $wallet->getCurrencies($user);
+        }
+
+        return new JsonResponse($params);
     }
 }
